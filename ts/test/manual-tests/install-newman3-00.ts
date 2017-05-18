f75c60526e1b9f5bdf9e123962b204173d12ea98
nobody.nobody <nobody@nowhere.com>
2017-05-18 10:12:40 -0500

    Break out ubuntu 16 server & desktop config, add newman install

---
+++ ts/test/manual-tests/install-newman3-00.ts
@@ -0,0 +1,13 @@
+import 'reflect-metadata';
+import kernel from '../../inversify.config';
+import {ProcessCommandJson} from '../../interfaces/process-command-json';
+import {RemoteCatalogGetter} from "firmament-yargs";
+let processCommandJson = kernel.get<ProcessCommandJson>('ProcessCommandJson');
+
+const installNewman3 = '/home/jreeme/src/firmament-bash/command-json/install-newman3-00.json';
+processCommandJson.processJson(
+  {
+    input: installNewman3
+  }
+);
+

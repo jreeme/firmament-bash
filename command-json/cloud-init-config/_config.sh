16142a66e1ff78f45a813ef78b34ce2a1fa6bc06
John <john.reeme@gmail.com>
2018-03-05 11:57:14 -0600

    Slowly patching VM _config.sh for openstack base VM

--- command-json/cloud-init-config/_config.sh
+++ command-json/cloud-init-config/_config.sh
@@ -4,4 +4,10 @@
 sudo sed "1s/$/ ubuntu/" -i /etc/hosts
 
 #Change
-sudo bash -c "echo 'vm.max_map_count=262144 >> /etc/sysctl.conf'"
+sudo bash -c 'echo vm.max_map_count=262144 >> /etc/sysctl.conf'
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+
